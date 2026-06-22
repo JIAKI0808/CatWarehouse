027dbf1783c1d07069c97cebfbd5cd3996f0cff3
JiaKi <1317619413@qq.com>
2026-06-22 19:05:06 +0800

    feat: implement collapsible side panel for categories

--- Cw_WebUi/src/views/HomeView.vue
+++ Cw_WebUi/src/views/HomeView.vue
@@ -1,34 +1,45 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { NButton, NIcon, NDrawer, NDrawerContent } from 'naive-ui'
-import { MenuOutline } from '@vicons/ionicons5'
+import { NIcon } from 'naive-ui'
+import { ChevronForwardOutline, ChevronBackOutline } from '@vicons/ionicons5'
 import { useItemStore } from '@/stores/item'
 import CategoryTree from '@/components/CategoryTree.vue'
 import ItemTable from '@/components/ItemTable.vue'
 
 const itemStore = useItemStore()
-const showDrawer = ref(false)
+const panelExpanded = ref(true)
 </script>
 
 <template>
-  <div class="h-full flex flex-col">
-    <div class="h-10 border-b bg-white flex items-center px-4 gap-4">
-      <NButton text @click="showDrawer = true">
-        <template #icon>
-          <NIcon :size="20"><MenuOutline /></NIcon>
-        </template>
-      </NButton>
-      <span class="text-sm font-medium text-gray-600">库存管理</span>
+  <div class="h-full flex">
+    <!-- 左侧分类面板 -->
+    <div
+      class="border-r bg-white flex flex-col transition-all duration-300"
+      :class="panelExpanded ? 'w-64' : 'w-10'"
+    >
+      <!-- 折叠按钮 -->
+      <div
+        class="h-10 border-b flex items-center justify-center cursor-pointer hover:bg-gray-100"
+        @click="panelExpanded = !panelExpanded"
+      >
+        <NIcon :size="16" class="text-gray-500">
+          <ChevronBackOutline v-if="panelExpanded" />
+          <ChevronForwardOutline v-else />
+        </NIcon>
+      </div>
+
+      <!-- 分类树 -->
+      <div
+        v-show="panelExpanded"
+        class="flex-1 overflow-hidden"
+      >
+        <CategoryTree />
+      </div>
     </div>
 
+    <!-- 右侧内容区域 -->
     <div class="flex-1 overflow-hidden bg-gray-50">
       <ItemTable />
     </div>
-
-    <NDrawer v-model:show="showDrawer" placement="left" :width="280">
-      <NDrawerContent>
-        <CategoryTree />
-      </NDrawerContent>
-    </NDrawer>
   </div>
 </template>
