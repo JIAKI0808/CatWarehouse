463ec0e08f12f62107a1bb36230e6661056bb855
JiaKi <1317619413@qq.com>
2026-06-22 18:23:16 +0800

    feat: update app configuration and views

--- Cw_WebUi/src/router/index.ts
+++ Cw_WebUi/src/router/index.ts
@@ -7,14 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  ],
 })
 
 export default router
--- Cw_WebUi/src/views/HomeView.vue
+++ Cw_WebUi/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
+import { NLayout, NLayoutSider, NLayoutContent } from 'naive-ui'
+import { useItemStore } from '@/stores/item'
+import CategoryTree from '@/components/CategoryTree.vue'
+import ItemTable from '@/components/ItemTable.vue'
+import ItemCard from '@/components/ItemCard.vue'
+import ViewToggle from '@/components/ViewToggle.vue'
+
+const itemStore = useItemStore()
 </script>
 
 <template>
-  <main>
-    <HelloWorld msg="CatWarehouse" />
-  </main>
+  <NLayout has-sider class="h-screen">
+    <NLayoutSider bordered :width="280">
+      <CategoryTree />
+    </NLayoutSider>
+    <NLayoutContent>
+      <div class="p-4 h-full flex flex-col">
+        <div class="mb-4 flex justify-end">
+          <ViewToggle />
+        </div>
+        <div class="flex-1 overflow-hidden">
+          <ItemTable v-if="itemStore.viewMode === 'table'" />
+          <ItemCard v-else />
+        </div>
+      </div>
+    </NLayoutContent>
+  </NLayout>
 </template>
